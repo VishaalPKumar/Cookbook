4a8818b99038f91b5ae628bb0794fbdf6831815c
Vishaal Kumar <59760349+VishaalPKumar@users.noreply.github.com>
2024-07-28 20:57:33 +0400

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Vishaal Kumar on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
